c9a15f1689062e5065e71e3ad963a242b2038002
Ahmed Ehab Abdelghany <102571403+AhmedEhabAbdelghany@users.noreply.github.com>
2023-04-28 18:47:41 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-let navsymbol    = document.getElementById("navsymbol");
-let sidenav      = document.getElementById("sidenav");
-let closesymbol  = document.getElementById("closesymbol");
+// Side Nav
+let navsymbol        = document.getElementById("navsymbol");
+let sidenav          = document.getElementById("sidenav");
+let closesymbol      = document.getElementById("closesymbol");
 
 navsymbol.addEventListener("click",function(){
   sidenav.style.display = "block"
@@ -8,4 +9,17 @@ navsymbol.addEventListener("click",function(){
 
 closesymbol.addEventListener("click",function(){
   sidenav.style.display = "none"
+})
+
+
+// Cart
+let cartsymbol   = document.getElementById("cartsymbol")
+let shopingcartt = document.getElementById("shopingcartt")
+let closecart    = document.getElementById("closecart")
+cartsymbol.addEventListener("click",function(){
+  shopingcartt.style.display = "block"
+})
+
+closecart.addEventListener("click",function(){
+  shopingcartt.style.display = "none"
 })
