c3c86c389d4f0e924aaabe9b21a9c138f5166118
Ahmed Ehab Abdelghany <102571403+AhmedEhabAbdelghany@users.noreply.github.com>
2023-05-18 05:06:44 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,10 +5,13 @@ let closesymbol      = document.getElementById("closesymbol");
 
 navsymbol.addEventListener("click",function(){
   sidenav.style.display = "block"
+  document.body.style.overflow = "hidden";
+
 })
 
 closesymbol.addEventListener("click",function(){
   sidenav.style.display = "none"
+  document.body.style.overflow = "scroll";
 })
 
 
@@ -18,10 +21,14 @@ let shoppingcart = document.getElementById("shoppingcart")
 let closecart    = document.getElementById("closecart")
 cartsymbol.addEventListener("click",function(){
   shoppingcart.style.display = "block"
+  document.body.style.overflow = "hidden";
+
 })
 
 closecart.addEventListener("click",function(){
   shoppingcart.style.display = "none"
+  document.body.style.overflow = "scroll";
+
 })
 
 
